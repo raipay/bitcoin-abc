af3bfc7c536c1f302275b328c340bd7974f99536
Tobias Ruck <ruck.tobias@gmail.com>
2023-05-09 00:15:43 +0200

    add Slpv2Writer::clear

--- chronik/chronik-db/src/slpv2/io.rs
+++ chronik/chronik-db/src/slpv2/io.rs
@@ -222,6 +222,13 @@ impl<'a> Slpv2Writer<'a> {
         Ok(())
     }
 
+    pub fn clear(&self, batch: &mut WriteBatch) -> Result<()> {
+        batch.delete_range_cf(self.col.cf_tx_data, b"".as_ref(), &[0xff; 16]);
+        batch.delete_range_cf(self.col.cf_token_meta, b"".as_ref(), &[0xff; 16]);
+        batch.delete_range_cf(self.col.cf_genesis_data, b"".as_ref(), &[0xff; 16]);
+        Ok(())
+    }
+
     /// Add the column families used for SLPv2.
     pub(crate) fn add_cfs(columns: &mut Vec<ColumnFamilyDescriptor>) {
         columns.push(ColumnFamilyDescriptor::new(
