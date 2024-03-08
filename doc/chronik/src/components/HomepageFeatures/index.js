7fdf4b5362d7fcc6d7d7f0e83f6bb157b3019832
Tobias Ruck <ruck.tobias@gmail.com>
2024-03-08 21:05:59 +0100

    [Chronik] Add Docusaurus documentation
    Summary:
    This provides a full reference on how to use Chronik, from downloading, to building, to setup and how to use `chronik-client`.
    
    This code is based on the auto-generated template tutorial for docusaurus.
    
    It uses mdx, which is a mix of Markdown and JSX, which gives us the ability to include Live Code so the user can play around with the code examples right there.
    
    Test Plan:
    1. in `modules/chronik-client`: `npm install`
    2. in `doc/chronik`: `npm install && npm serve`
    3. Should open browser automatically, go through all pages and check if the information is correct
    
    Reviewers: Fabien, #bitcoin_abc
    
    Differential Revision: https://reviews.bitcoinabc.org/D15665

---
+++ doc/chronik/src/components/HomepageFeatures/index.js
@@ -0,0 +1,67 @@
+// Copyright (c) 2024 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Chronik was designed from the ground up to be easy to use to get
+        your app using Chronik quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Chronik lets you focus on your app, Chronik will do all the heavy lifting.
+      </>
+    ),
+  },
+  {
+    title: 'Highly optimized',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        We spent a lot of time making Chronik the fastest blockchain indexer out there,
+        so you don't waste time re-indexing.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ doc/chronik/src/pages/index.js
@@ -0,0 +1,47 @@
+// Copyright (c) 2024 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Chronik Documentation
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description={siteConfig.tagline}>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
